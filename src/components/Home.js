de26ad84e2d184562e5c98c46846cef286a5a0c5
stiv120 <48624431+stiv120@users.noreply.github.com>
2024-08-08 18:21:52 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
-                <a className="navbar-brand">Api React with PHP</a>
+                <a className="navbar-brand">Api React con PHP</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false"
                     aria-label="Toggle navigation">
